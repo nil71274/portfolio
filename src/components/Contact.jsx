72e812858402910f9b1d38b6e17e646bf651d11b
Nilesh <nileshpawar290402@gmail.com>
2023-09-10 17:13:32 +0530

    contact working

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,8 +34,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_94u05hs',
+        'template_8joebcp',
         {
           from_name: form.name,
           to_name: "JavaScript Mastery",
@@ -43,7 +43,7 @@ const Contact = () => {
           to_email: "nilpawar187234@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'lRn8M2sTigoOhljyy'
       )
       .then(
         () => {
